947a478cc957ec76b5adccfb83f477f480e9b401
MOONLAPSED <MOONLAPSED@gmail.com>
2025-05-02 23:39:20 -0500

    The new invoke_setup workflow 'misses timing' even when launched from the start menu, fix this.

--- .platdist/invoke_setup.bat
+++ .platdist/invoke_setup.bat
@@ -1,4 +1,5 @@
 @echo off
 :: Windows-safe way to invoke the PowerShell script with bypass
+:: We 'miss the timing' of the sandbox startup-system
 powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%~dp0scoop_setup.ps1"
 exit /b %ERRORLEVEL%
